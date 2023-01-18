8755415fd8b24aaec8e90a4ab9fc5faacf1cc5be
Ziwei Huang <huang-ziwei@outlook.com>
2023-01-18 18:17:49 +0100

    fix build error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 exec(open("pycircstat2/version.py").read())
 setup(
     name="pycircstat2",
-    version=__version__,
+    version=__version__,  # noqa: F821
     description="Python toolbox for circular statistcs",
     author=["Ziwei Huang", "Philipp Berens"],
     author_email="huang-ziwei@outlook.com",
