5fcfc401331d87f636215a7b0c98b384a5262a3e
Ziwei Huang <huang-ziwei@outlook.com>
2025-09-27 15:06:06 +0200

    change: improve regression.py

--- pycircstat2/__init__.py
+++ pycircstat2/__init__.py
@@ -1,4 +1,12 @@
+from importlib import metadata as _metadata
+
 from .base import Axial, Circular
 from .utils import load_data
-from .version import __version__
 from .visualization import circ_plot
+
+try:  # Prefer installed package metadata
+    __version__ = _metadata.version("pycircstat2")
+except _metadata.PackageNotFoundError:  # pragma: no cover - during local dev
+    __version__ = "0.0.0"
+
+__all__ = ["Axial", "Circular", "circ_plot", "load_data", "__version__"]
